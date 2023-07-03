44eb43de1f2710f1427cbc485b7901228b72ae91
M-Ali-Alam <m.ali.14964@khi.iba.edu.pk>
2023-07-04 01:55:30 +0500

    created background of login screen

---
+++ client/src/constants/Color.ts
@@ -0,0 +1,8 @@
+import { teal } from "@mui/material/colors";
+
+export const TEAL_GREEN_DARK = teal.A400;
+export const TEAL_GREEN_LIGHT = "#128c7e";
+export const LIGHT_GREEN = "#25d366";
+export const TEA_GREEN = "#dcf8c6";
+export const BLUE = "#34b7f1";
+export const LIGHT_GRAY = "#ece5dd";
--- client/src/features/login/Login.tsx
+++ client/src/features/login/Login.tsx
@@ -1,10 +1,15 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box } from "@mui/material";
+import { green, grey } from "@mui/material/colors";
+import { TEAL_GREEN_DARK } from "../../constants/Color";
 
 const Login = () => {
     return (
-    <Box>
-        <Typography>This is going far beyond my capability to handle</Typography>
+    <Box bgcolor={grey[100]} height={"100vh"}>
+        <Box bgcolor={TEAL_GREEN_DARK} height={225}>
+
+        </Box>
+        <Box bgcolor={green[400]} >
+        </Box>
     </Box>
     )
 }
